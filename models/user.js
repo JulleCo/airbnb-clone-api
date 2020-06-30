f453e1d9289db6d331d7880a37a0a3173ecec03f
Gass <gwenaelle.fouquet@gmail.com>
2020-06-30 23:24:45 +0200

    sequelize-cli + models + migrations + ajout sur bd

--- models/user.js
+++ models/user.js
@@ -1,7 +1,9 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const Booking = require('../models/booking'),
+
+  const {
+    Model
+  } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     /**
@@ -12,15 +14,19 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
     }
-  };
+  }
   User.init({
+    id: DataTypes.INTEGER,
     firstName: DataTypes.STRING,
     lastName: DataTypes.STRING,
     password: DataTypes.TEXT,
-    firstRegistration: DataTypes.DATE
+    firstRegistration: DataTypes.DATE,
   }, {
     sequelize,
     modelName: 'User',
   });
+
+  User.Booking = User.hasMany(Booking);
+
   return User;
-};
+}
