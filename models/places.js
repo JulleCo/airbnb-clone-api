a9e3e300b3c267b7592426d7c16d5debe206f59b
Gass <gwenaelle.fouquet@gmail.com>
2020-07-03 16:31:17 +0200

    creation model places et migration places

---
+++ models/places.js
@@ -0,0 +1,30 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Places extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  Places.init({
+    idCity: DataTypes.INTEGER,
+    // idUser: DataTypes.INTEGER,
+    name: DataTypes.STRING,
+    description: DataTypes.STRING,
+    rooms: DataTypes.INTEGER,
+    bathrooms: DataTypes.INTEGER,
+    maxGuests: DataTypes.INTEGER,
+    priceByNight: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Places',
+  });
+  return Places;
+};
